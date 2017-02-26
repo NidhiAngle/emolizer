6e6b2aa1635b5aef116c5280debf62930941f1c1
NidhiAngle <nidhiangle94@gmail.com>
2017-02-26 13:36:08 -0500

    Update FM17.py

--- FM17.py
+++ FM17.py
@@ -1,21 +1,3 @@
-#!/usr/bin/env python
-# Copyright 2016 Google Inc. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""Google Cloud Speech API sample application using the REST API for batch
-processing.
-Example usage: python transcribe.py resources/audio.raw
-"""
 
 # [START import_libraries]
 import argparse
@@ -30,11 +12,8 @@ import googleapiclient.discovery
 
 
 def to_text(speech_file):
-    """Transcribe the given audio file.
-    Args:
-        speech_file: the name of the audio file.
-    """
-    # [START construct_request]
+    
+
     with open(speech_file, 'rb') as speech:
         # Base64 encode the binary audio file for inclusion in the JSON
         # request.
